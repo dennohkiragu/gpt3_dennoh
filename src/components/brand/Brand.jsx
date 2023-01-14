e9dfd7855837d111bda54b9cb87762212ab91222
dennohkiragu <denyzbleach@gmail.com>
2023-01-14 06:45:20 +0300

    header and brand logos

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import'./brand.css';
+import { google, slack, dropbox, shopify, atlassian } from "./import"
 
 const Brand = () => {
     return (
-        <div>
+        <div className="gpt3__brand section__padding">
+            <div><img src={google} alt="google"/></div>
+            <div><img src={slack} alt="google"/></div>
+            <div><img src={dropbox} alt="google"/></div>
+            <div><img src={shopify} alt="google"/></div>
+            <div><img src={atlassian} alt="google"/></div>
 
         </div>
     );
